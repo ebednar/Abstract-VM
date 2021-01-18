ea1477d8e4925990f83b97ff1b24385f731ba917
Karim Kirillov <prjajkin.infro@gmail.com>
2021-01-18 18:16:21 +0300

    main

---
+++ Abstract VM/Abstract VM/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+
+	}
+	else if (argc == 2)
+	{
+
+	}
+	else
+		std::cout << "usage: './avm' for standart input or './avm [file]' for input file" << std::endl;
+	return 0;
+}
